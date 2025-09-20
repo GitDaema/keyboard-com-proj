6a90f351d2eed725bb865e7103d92224dab4d978
GitDaema <bedelj3@gmail.com>
2025-09-20 23:12:13 +0900

    Compose keyboard presets for registers and memory

--- src/main.py
+++ src/main.py
@@ -13,21 +13,18 @@ OpenRGB 서버(127.0.0.1:6742)에 연결해 ESC 키만 제어하는 예제.
 import time
 from rgb_controller import connect, disconnect, set_key_color, get_key_color, init_all_keys
 from utils.bitgroups import set_group_value, get_group_value, copy_group_value
-from utils.keyboard_presets import BYTE_A, BYTE_B
+import utils.keyboard_presets as kp
 import utils.color_presets as cp
 
 def rgb_routine(label: str):
-    # ESC 색상 순환(빨-초-파-끄기)
-    for c in [cp.RED, cp.GREEN, cp.BLUE, cp.BLACK]:
-        # 실제 장치의 현재색을 읽어 확인
-        print(label, "현재색 =", get_key_color(label, fresh=True))
-        time.sleep(0.3)
-        set_key_color(label, c, debug=True)
+    for c in cp.HEX_COLORS.values():
         time.sleep(0.6)
+        set_key_color(label, c, debug=True)
+        input()
 
 def bitgroup_test(n: int):
     time.sleep(0.6)
-    set_group_value(BYTE_A, n, on_color=cp.GREEN, off_color=cp.DARK_RED, debug = True)
+    # set_group_value(kp.BYTE_A, n, on_color=cp.GREEN, off_color=cp.DARK_RED, debug = True)
     # val, bits, on_labels = get_group_value(BYTE_A, debug=True)
     # print("[CHECK] value=", val, "bits(LSB→)=", bits, "on=", on_labels)
     # input()
@@ -39,14 +36,7 @@ def main():
         print("[INFO] All keys ready. Press Enter key to start.")
         input()
 
-        bitgroup_test(100)
-        input()
-
-        copy_group_value(BYTE_A, BYTE_B,
-                 on_color=cp.GREEN,
-                 off_color=cp.DARK_RED,
-                 debug=True)
-        input()
+        rgb_routine("esc")
         
     finally:
         init_all_keys()
--- src/utils/color_presets.py
+++ src/utils/color_presets.py
@@ -10,3 +10,27 @@ PURPLE      = RGBColor(170,   0, 170)
 PINK        = RGBColor(255, 105, 180)
 WHITE       = RGBColor(255, 255, 255)
 BLACK       = RGBColor(  0,   0,   0)
+
+HEX_COLORS = {
+    0x0: RGBColor(0, 0, 0),          # Black
+
+    # 1~7: Dark ROYGBIV
+    0x1: RGBColor(128, 0,   0),      # Dark Red
+    0x2: RGBColor(128, 64,  0),      # Dark Orange
+    0x3: RGBColor(128,110,  0),      # Dark Yellow
+    0x4: RGBColor(0, 110,  0),       # Dark Green
+    0x5: RGBColor(0,  60, 128),      # Dark Blue
+    0x6: RGBColor(38,  0,  65),      # Dark Indigo
+    0x7: RGBColor(74,  0, 105),      # Dark Violet
+
+    # 8~14: Bright ROYGBIV
+    0x8:  RGBColor(255, 0,   0),     # Bright Red
+    0x9:  RGBColor(255,128,  0),     # Bright Orange
+    0xA:  RGBColor(255,220,  0),     # Bright Yellow
+    0xB:  RGBColor(0,  220,  0),     # Bright Green
+    0xC:  RGBColor(0,  120,255),     # Bright Blue
+    0xD:  RGBColor(75,   0,130),     # Bright Indigo
+    0xE:  RGBColor(148,  0,211),     # Bright Violet
+
+    0xF: RGBColor(255,255,255),      # White
+}
